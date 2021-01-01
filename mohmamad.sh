27a2d57d4d815105ab22abf720c5e6b01750ed74
Facebook penetration <65963511+mohammadkoko1993@users.noreply.github.com>
2021-01-01 15:00:06 +0200

    Add files via upload

---
+++ mohmamad.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+now=`date`
+#echo "Please enter your name" 
+names="mohammad"
+sleep 2
+echo "Hello" $names
+espeak "Hello"""$names
+echo " love you" $names
+espeak "love you"""$names
+echo "today : $now"
+espeak "today : $now"
+echo "Happy Hacker " $names
+espeak "Happy Hacker"""$names
+
